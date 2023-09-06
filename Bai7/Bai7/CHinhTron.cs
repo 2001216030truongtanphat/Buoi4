907757d32559bb06ceb17c6180122520df52b52f
Phat <hufitp@gmail.com>
2023-09-06 15:03:42 +0700

    add bai7

---
+++ Bai7/Bai7/CHinhTron.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7
+{
+    class CHinhTron
+    {
+        private CToaDo _tam;
+        public CToaDo Tam
+        {
+            get { return _tam; }
+            set { _tam = value; }
+        }
+        private double banKinh;
+        public double BanKinh
+        {
+            get { return banKinh; }
+            set { banKinh = value; }
+        }
+        public CHinhTron()
+        {
+            this.Tam = new CToaDo();
+            BanKinh = 0;
+        }
+        public CHinhTron(CToaDo t, double bk)
+        {
+            this.Tam = t;
+            this.Tam.X = t.X;
+            this.Tam.Y = t.Y;
+            this.Tam.Ten = t.Ten;
+            this.BanKinh = bk;
+        }
+        public double tinhChuVi()
+        {
+            return BanKinh * 2 * 3.14;
+        }
+        public double tinhDienTich()
+        {
+            return BanKinh * BanKinh * 3.14;
+        }
+
+    }
+}
---
+++ Bai7/Bai7/Program.cs
@@ -0,0 +1,20 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var td = new CToaDo(5, 5, "O");
+            var ht = new CHinhTron(td , 10.5);
+            Console.WriteLine("Hinh tron co tam {0} voi ban kinh {1} co dien tich va chu vi lan luot la {2} va {3}", 
+                ht.Tam.ToString(),ht.BanKinh,ht.tinhDienTich(),ht.tinhChuVi());
+            Console.ReadKey();
+        }
+    }
+}
